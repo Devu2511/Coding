2fb0d29c8dcd0c2e6b3147462860ba39af8672a8
Divyansh <devu7693@gmail.com>
2023-06-02 15:13:00 +0530

    adding two sums

---
+++ Array/Easy/Twosums.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i=0,j=nums.size()-1;
+        vector<pair<int,int>>ques;
+        for(int z=0;z<nums.size();z++)
+        {
+            ques.push_back(make_pair(nums[z],z));
+        }
+        sort(ques.begin(),ques.end());
+        while(i<j)
+        {
+            int sum=ques[i].first + ques[j].first;
+            if(sum==target)break;
+            else if(sum>target)j--;
+            else i++;
+        }
+        vector<int>result={ques[i].second,ques[j].second};
+        return result;
+    }
+};
